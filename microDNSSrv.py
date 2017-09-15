2ea49904d0d4bc781d200a91384e053fdd736221
jczic <jczic@free.fr>
2017-09-15 05:59:15 +0200

    Fix utils functions

--- microDNSSrv.py
+++ microDNSSrv.py
@@ -16,6 +16,77 @@ class MicroDNSSrv :
         return None
 
     # ============================================================================
+    # ===( Utils )================================================================
+    # ============================================================================
+
+    def _tryStartThread(func, args=()) :
+        for x in range(10) :
+            try :
+                gc.collect()
+                start_new_thread(func, args)
+                return True
+            except :
+                pass
+        return False
+
+    # ----------------------------------------------------------------------------
+
+    def _ipV4StrToBytes(ipStr) :
+        try :
+            parts = ipStr.split('.')
+            if len(parts) == 4 :
+                return bytes( [ int(parts[0]),
+                                int(parts[1]),
+                                int(parts[2]),
+                                int(parts[3]) ] )
+        except :
+            pass
+        return None
+
+    # ----------------------------------------------------------------------------
+
+    def _getAskedDomainName(packet) :
+        try :
+            queryType = (packet[2] >> 3) & 15
+            qCount    = (packet[4] << 8) | packet[5]
+            if queryType == 0 and qCount == 1 :
+                pos     = 12
+                domName = ''
+                while True :
+                    domPartLen = packet[pos]
+                    if (domPartLen == 0) :
+                        break
+                    domName += ('.' if len(domName) > 0 else '') \
+                             + packet[ pos+1 : pos+1+domPartLen ].decode()
+                    pos     += 1+domPartLen
+                return domName
+        except :
+            pass
+        return None
+
+    # ----------------------------------------------------------------------------
+
+    def _getPacketAnswerA(packet, ipV4Bytes) :
+        try :
+            return b''.join( [
+                packet[:2],             # Query identifier
+                b'\x85\x80',            # Flags and codes
+                packet[4:6],            # Query question count
+                b'\x00\x01',            # Answer record count
+                b'\x00\x00',            # Authority record count
+                b'\x00\x00',            # Additional record count
+                packet[12:],            # Query question
+                b'\xc0\x0c',            # Answer name as pointer
+                b'\x00\x01',            # Answer type A
+                b'\x00\x01',            # Answer class IN
+                b'\x00\x00\x00\x1E',    # Answer TTL 30 secondes
+                b'\x00\x04',            # Answer data length
+                ipV4Bytes ] )           # Answer data
+        except :
+            pass
+        return None
+
+    # ============================================================================
     # ===( Constructor )==========================================================
     # ============================================================================
 
@@ -32,13 +103,13 @@ class MicroDNSSrv :
         while True :
             try :
                 packet, cliAddr = self._server.recvfrom(256)
-                domName = self._getAskedDomainName(packet)
+                domName = MicroDNSSrv._getAskedDomainName(packet)
                 if domName :
                     ipB = self._domList.get(domName.lower(), None)
                     if not ipB :
                         ipB = self._domList.get('*', None)
                     if ipB :
-                        packet = self._getPacketAnswerA(packet, ipB)
+                        packet = MicroDNSSrv._getPacketAnswerA(packet, ipB)
                         if packet :
                             self._server.sendto(packet, cliAddr)
             except :
@@ -59,7 +130,7 @@ class MicroDNSSrv :
                                      1 )
             self._server.bind(('0.0.0.0', 53))
             self._server.setblocking(True)
-            return _tryStartThread(self._serverProcess)
+            return MicroDNSSrv._tryStartThread(self._serverProcess)
         return False
 
     # ----------------------------------------------------------------------------
@@ -83,7 +154,7 @@ class MicroDNSSrv :
             o = { }
             for dom, ip in domainsList.items() :
                 if isinstance(dom, str) and len(dom) > 0 :
-                    ipB = self._ipV4StrToBytes(ip)
+                    ipB = MicroDNSSrv._ipV4StrToBytes(ip)
                     if ipB :
                         o[dom.lower()] = ipB
                         continue
@@ -92,77 +163,6 @@ class MicroDNSSrv :
                 self._domList = o
                 return True
         return False
-
-    # ============================================================================
-    # ===( Utils )================================================================
-    # ============================================================================
-
-    def _tryStartThread(self, func, args=()) :
-        for x in range(10) :
-            try :
-                gc.collect()
-                start_new_thread(func, args)
-                return True
-            except :
-                pass
-        return False
-
-    # ----------------------------------------------------------------------------
-
-    def _ipV4StrToBytes(self, ipStr) :
-        try :
-            parts = ipStr.split('.')
-            if len(parts) == 4 :
-                return bytes( [ int(parts[0]),
-                                int(parts[1]),
-                                int(parts[2]),
-                                int(parts[3]) ] )
-        except :
-            pass
-        return None
-
-    # ----------------------------------------------------------------------------
-
-    def _getAskedDomainName(self, packet) :
-        try :
-            queryType = (packet[2] >> 3) & 15
-            qCount    = (packet[4] << 8) | packet[5]
-            if queryType == 0 and qCount == 1 :
-                pos     = 12
-                domName = ''
-                while True :
-                    domPartLen = packet[pos]
-                    if (domPartLen == 0) :
-                        break
-                    domName += ('.' if len(domName) > 0 else '') \
-                             + packet[ pos+1 : pos+1+domPartLen ].decode()
-                    pos     += 1+domPartLen
-                return domName
-        except :
-            pass
-        return None
-
-    # ----------------------------------------------------------------------------
-
-    def _getPacketAnswerA(self, packet, ipV4Bytes) :
-        try :
-            return b''.join( [
-                packet[:2],             # Query identifier
-                b'\x85\x80',            # Flags and codes
-                packet[4:6],            # Query question count
-                b'\x00\x01',            # Answer record count
-                b'\x00\x00',            # Authority record count
-                b'\x00\x00',            # Additional record count
-                packet[12:],            # Query question
-                b'\xc0\x0c',            # Answer name as pointer
-                b'\x00\x01',            # Answer type A
-                b'\x00\x01',            # Answer class IN
-                b'\x00\x00\x00\x1E',    # Answer TTL 30 secondes
-                b'\x00\x04',            # Answer data length
-                ipV4Bytes ] )           # Answer data
-        except :
-            pass
-        return None
 
     # ============================================================================
     # ============================================================================
