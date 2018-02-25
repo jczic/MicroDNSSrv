65b1b3c4ff7105886e81e599c3282d7bf298dcf6
J-Christophe Bos <jczic.bos@gmail.com>
2018-02-25 10:23:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ print()
 
 WLAN().init(mode=WLAN.AP, ssid='Test MicroDNSSrv', auth=(WLAN.WPA2, 'azerty123'))
 
-srv = MicroDNSSrv.Create( {
-	"*test.com"	 : "123.123.123.123",
-	"*google*"   : "192.168.4.1",
-	"*.toto.com" : "192.168.4.1" } )
-
-if srv :
-	print("MicroDNSSrv started.")
+if MicroDNSSrv.Create( {
+  "test.com"   : "1.1.1.1",
+  "*test2.com" : "2.2.2.2",
+  "*google*"   : "192.168.4.1",
+  "*.toto.com" : "192.168.4.1",
+  "www.site.*" : "192.168.4.1" } ) :
+  print("MicroDNSSrv started.")
 else :
-	print("Error to start MicroDNSSrv...")
+  print("Error to starts MicroDNSSrv...")
 
 print()
 print("=======================================================================")
